424a40a6accbb317f3ec80d8cbc9938403e30b2c
Pawan Sadhwani <pawan.sadhwani@wolkus.com>
2026-05-04 22:32:59 +0530

    added ingestion - transformation

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 
-logs_dir = os.path.join(os.getcwd(), "logs")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+logs_dir = os.path.join(BASE_DIR, "..", "logs")
 os.makedirs(logs_dir, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-if __name__ == "__main__":
-    logging.info("logging has started")
